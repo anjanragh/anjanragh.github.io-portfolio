3dcf9cb86e332a985d32a49aae53fbf748102692
anjanragh <anjanragh@gmail.com>
2020-09-26 03:14:40 -0400

    Updates

---
+++ precache-manifest.4fbbb9ee427e1458df08e3079de501ec.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7855400bb06d7615356d27b6b787701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d8b8b64e07090ff548e",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "12db8ad72c6a6c18deda",
+    "url": "/static/css/3.985f1a72.chunk.css"
+  },
+  {
+    "revision": "4c1e9f2730236f537fd5",
+    "url": "/static/css/4.c704da77.chunk.css"
+  },
+  {
+    "revision": "ae43eca55c96b6a10e4b",
+    "url": "/static/css/6.910b14d5.chunk.css"
+  },
+  {
+    "revision": "36f56875f54f92a8c409",
+    "url": "/static/css/7.62326089.chunk.css"
+  },
+  {
+    "revision": "dbe26954067f04446a84",
+    "url": "/static/css/main.3bb85033.chunk.css"
+  },
+  {
+    "revision": "4d8b8b64e07090ff548e",
+    "url": "/static/js/2.431da12f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.431da12f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12db8ad72c6a6c18deda",
+    "url": "/static/js/3.50152851.chunk.js"
+  },
+  {
+    "revision": "4c1e9f2730236f537fd5",
+    "url": "/static/js/4.2e264d2b.chunk.js"
+  },
+  {
+    "revision": "596399f6bd67268c0384",
+    "url": "/static/js/5.fe265095.chunk.js"
+  },
+  {
+    "revision": "ae43eca55c96b6a10e4b",
+    "url": "/static/js/6.74bf33c7.chunk.js"
+  },
+  {
+    "revision": "36f56875f54f92a8c409",
+    "url": "/static/js/7.0e17dee6.chunk.js"
+  },
+  {
+    "revision": "dbe26954067f04446a84",
+    "url": "/static/js/main.acc2da73.chunk.js"
+  },
+  {
+    "revision": "e51108ef7661a97cc500",
+    "url": "/static/js/runtime-main.ec6c50c8.js"
+  },
+  {
+    "revision": "6089395c06d90102cf4a0579ab50dec5",
+    "url": "/static/media/logo.6089395c.png"
+  },
+  {
+    "revision": "3200b262b7ab03daf73870dfa11eb956",
+    "url": "/static/media/person.3200b262.png"
+  }
+]);
